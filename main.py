a29a1f41ef36781320f550182e6582a49a184b2b
itweb <redroza@yandex.ru>
2026-02-18 23:11:26 +0500

    fixes

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from src.widget import mask_account_card, get_date
+from src.processing import filter_by_state, sort_by_date
 
 card_masked = mask_account_card("MasterCard 7158300734726758")
 print(card_masked)
 
 date_formatted = get_date("2023-12-31T23:59:59.999999")
 print(date_formatted)
-
-from src.processing import filter_by_state, sort_by_date
 
 operations = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
