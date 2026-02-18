193be126099aec6b36b4f73fad4ef875273c0f6b
itweb <redroza@yandex.ru>
2026-02-18 14:06:59 +0500

    init

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card, get_date
 
-print(get_mask_account('12345678912345678912'))
-print(get_mask_card_number('1234567891234567'))
+card_masked = mask_account_card("MasterCard 7158300734726758")
+print(card_masked)
+
+date_formatted = get_date("2023-12-31T23:59:59.999999")
+print(date_formatted)
